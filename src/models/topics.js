c80829c91970dbff3d5000b3d209c6d8056e671b
paez <kamilopaez19@gmail.com>
2019-07-29 20:48:08 -0500

    avance

--- src/models/topics.js
+++ src/models/topics.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const {Schema}= mongoose;
 
 const TopicSchema = new Schema({
+    username: {type: String, required: true},
     title: {type:String, required:true},
-    description: {type:String, required:true}
+    description: {type:String, required:true},
+    date: {type:Date, required:true},
+    comments: {type:Array, required:false}
 })
 
 module.exports = mongoose.model('Topic', TopicSchema);
