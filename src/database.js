e6ea2f3dc95930ce88f0b0a3d575cf7f7e683dd0
paez <kamilopaez19@gmail.com>
2019-08-11 13:21:15 -0500

    removing

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/octopus';
-
-mongoose.connect(URI,{useNewUrlParser: true})
-    .then(db => console.log("db conected"))
-    .catch(err => console.error(err));
-
-module.exports = mongoose;
