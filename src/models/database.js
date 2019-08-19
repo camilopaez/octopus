b83bb7330c2b84159cb1741ec4b4e069c95f2fc5
paez <kamilopaez19@gmail.com>
2019-08-19 01:57:47 -0500

    ultimo

--- src/models/database.js
+++ src/models/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//const URI = 'mongodb://db:27017/octopus';
-const URI = 'mongodb://localhost/octopus';
+const URI = 'mongodb://db:27017/octopus';
+//const URI = 'mongodb://localhost/octopus';
 mongoose.connect(URI,{useNewUrlParser: true})
     .then(db => console.log("db conected"))
     .catch(err => console.error(err));
